81c110a1a4f3761edc058ee35ad16d36da267d0f
Ming <hello.david.me@gmail.com>
2018-03-21 14:34:29 +0800

    Initial Commit

---
+++ PartitionSlider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PartitionSlider
+//
+//  Created by Ming on 2018/3/21.
+//  Copyright © 2018年 David.Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
